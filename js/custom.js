2163593be609d5e8c1e342b7007ca2af34acef71
NayanKhan <i4u.nayan@gmail.com>
2022-07-29 21:29:38 +0600

    My Project

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+  $('.location_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        prevArrow: '<i class="fa fa-angle-left slidPrv"></i>',
+        nextArrow: '<i class="fa fa-angle-right slidNext"></i>',
+        arrows: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+    });
+
+
